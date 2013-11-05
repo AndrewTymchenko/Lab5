16fda033d5b1801cb09eae2cb6b8bbd2f5a6ef49
C15Andrew.Tymchenko@usafa.edu <C15Andrew.Tymchenko@C15ALTymchenko.usafa.ds.af.edu>
2013-11-04 20:34:54 -0700

    time to commit

---
+++ LCD.h
@@ -0,0 +1,14 @@
+/*
+ * LCD.h
+ *
+ *  Created on: Nov 4, 2013
+ *      Author: C15Andrew.Tymchenko
+ */
+
+#ifndef LCD_H_
+#define LCD_H_
+
+
+
+
+#endif /* LCD_H_ */
---
+++ game.c
@@ -0,0 +1,45 @@
+/*
+ * game.c
+ *
+ *  Created on: Nov 4, 2013
+ *      Author: C15Andrew.Tymchenko
+ */
+
+#include <msp430g2553.h>
+#include "game.h"
+#include "LCD/LCD.h"
+
+unsigned char initPlayer()
+{
+        return 0x80;
+}
+
+void printPlayer(unsigned char player)
+{
+        writeCommandByte(player);
+        writeDataByte('*');
+}
+
+void clearPlayer(unsigned char player)
+{
+        writeCommandByte(player);
+        writeDataByte(' ');
+}
+
+unsigned char movePlayer(unsigned char player, unsigned char direction)
+{
+        switch (direction) {
+                //
+                // update player position based on direction of movement
+                //
+        }
+
+        return player;
+}
+
+char didPlayerWin(unsigned char player)
+{
+        return player == 0xC7;
+}
+
+
